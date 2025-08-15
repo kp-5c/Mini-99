47e4ba4e64b829f891710f16476c643c328049b5
Ebenoist <emma.benoist91@gmail.com>
2025-08-15 20:22:11 +0200

    Add files via upload
    git push 1508

--- minishell/srcs/main/main.c
+++ minishell/srcs/main/main.c
@@ -2,27 +2,36 @@
 
 int	g_signal_status = 0;
 
-int main(int ac, char **av, char **envp)
+int	main(int ac, char **av, char **envp)
 {
-    t_data  *data;
+	t_data	*data;
+	//int fd = open("text.txt", O_RDONLY);
+	//char buffer[120];
 
-    data = _data();
-    ac = 0;
-    av = NULL;
-    envp = NULL;
-    while (1)
-    {
-        data->input = readline("minishell$ ");
-        if (!data->input)
-        {
-            write(STDOUT_FILENO, "exit\n", 5);
-            break;
-        }
-        if (data->input)
-            add_history(data-> input);
-        free(data->input);
-        write (1, "test\n", 5);
-    }
-    last_free();
-    return (0);
+	data = _data();
+	ac = 0;
+	av = NULL;
+	envp = NULL;
+	while (1)
+	{
+		read_history("text.txt");
+		data->input = readline("minishell$ ");
+		write_history("text.txt");
+		if (!data->input)
+		{
+			write(STDOUT_FILENO, "exit\n", 5);
+			break ;
+		}
+		if (data->input)
+		{
+			add_history(data->input);
+			if (!parsing(data->input))
+				write(2, "bad command line\n", 18);
+		}
+		write_history("text.txt");
+		clear_history();
+		free(data->input);
+	}
+	last_free();
+	return (0);
 }
